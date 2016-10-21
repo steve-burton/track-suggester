63f897caee9c0ec5cc6868cd84791cfb6a63b42f
steve-burton <stevevburton@gmail.com>
2016-10-21 10:27:44 -0700

    fixes javascript variables

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,25 @@ $(document).ready(function() {
   $("form#questions").submit(function(event) {
     var website = $("select#website").val();
     var favoriteColor = $("select#favoriteColor").val();
-    var computer = $("input#radio").val();
+    var computer = $("input:radio[name=computer]:checked").val();
     var hipster = $("select#hipster").val();
     var sunday = $("select#sunday").val();
 
 
   var yourtrack = function(){
 
-      if (website === "frontEnd" && favoriteColor === "orange") {
+      if (website == "frontEnd" || favoriteColor == "orange") {
         return ("CSS/Design");
-      } else if (favoriteColor === "orange") {
-        return ("CSS/Design");
-      } else if (computer === "windows") {
-        return ("C#/.NET");
-      } else if (hipster === "exactly") {
-        return ("Ruby/Rails");
-      } else if (sunday === "football") {
-        return ("C#/.NET");
-      } else (sunday === "sleep") {
-        return ("Ruby/Rails");
+        } else if (favoriteColor == "orange") {
+          return ("CSS/Design");
+        } else if (computer == "windows") {
+          return ("C#/.NET");
+        } else if (hipster == "exactly") {
+          return ("Ruby/Rails");
+        } else if (sunday == "football") {
+          return ("C#/.NET");
+        } else {
+          return ("Ruby/Rails");
       }
     };
 
